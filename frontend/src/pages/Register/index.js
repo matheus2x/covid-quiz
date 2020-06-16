e5f859b5fda45702589a50d06f8978f601d7f17d
Matheus Henrique <matheushenriqueplus99@gmail.com>
2020-06-16 00:07:02 -0300

    add first half of Register page

--- frontend/src/pages/Register/index.js
+++ frontend/src/pages/Register/index.js
@@ -8,9 +8,30 @@ import "./styles.css";
 const Register = () => {
   return (
     <div className="register-container">
-      <Link to="/">
-        <Logo virusSize={75} covidSize={45} quizSize={31} />
-      </Link>
+      <header className="register-header">
+        <Link to="/">
+          <Logo virusSize={75} covidSize={45} quizSize={31} />
+        </Link>
+        <h1 className="register">Cadastro</h1>
+      </header>
+
+      <form className="form-locale">
+        <div className="select-group">
+          <select name="country" id="country">
+            <option value="0">País</option>
+          </select>
+          <select name="state" id="state">
+            <option value="0">Estado ou Província</option>
+          </select>
+        </div>
+
+        <div className="button-group">
+          <Link to="/login">
+            <button type="reset">Cancelar</button>
+          </Link>
+          <button type="submit">Avançar</button>
+        </div>
+      </form>
     </div>
   );
 };
